038a41691bfb6ce0d09c9fc12d419c7dc58a8d61
huyen120495 <huyen120495@gmail.com>
2018-02-26 15:58:05 +0700

    edit delete and add middleware.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+const middleware = require('./middleware');
 
 const app = express();
 
@@ -22,38 +23,38 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.listen(3000);
 
 app.get('/books', function(req, res) {
-    connection.query('select * from book', function(error, results) {
+    connection.query('select * from book where isdeleted = 0', function(error, results) {
         if (error) throw error;
         res.end(JSON.stringify(results));
     });
 });
 
 app.get('/book/:id', function(req,res) {
-    connection.query('select * from book where id = ?', [req.params.id], function(error, results) {
+    connection.query('select * from book where id = ? and isdeleted = 0', [req.params.id], function(error, results) {
         if (error) throw error;
         res.end(JSON.stringify(results));
     });
 });
 
-app.post('/book', function(req, res) {
+app.post('/book', middleware, function(req, res) {
     connection.query('insert into book set ?', req.body, function(error, results) {
         if (error) throw error;
-        res.end(JSON.stringify(results));
+        res.status(201).send({message : 'created'});
     });
 });
 
 app.put('/book', function(req, res) {
-    let sql = 'update book set title = ?, author = ?, publister = ?, price = ? where id = ?';
+    let sql = 'update book set title = ?, author = ?, publister = ?, price = ? where id = ? and isdeleted = 0';
     connection.query(sql, [req.body.title, req.body.author, req.body.publister, req.body.price, req.body.id],
         function(error, results) {
         if (error) throw error;
-        res.end(JSON.stringify(results));
+        res.send({message : 'modify'});
     });
 });
 
 app.delete('/book', function(req, res) {
-    connection.query('delete from book where id = ?', req.body.id, function(error, results) {
+    connection.query('update book set isdeleted = 1 where id = ?', req.body.id, function(error, results) {
         if (error) throw error;
-        res.end(JSON.stringify(results));
+        res.send({message : 'deleted'});
     })
 });
